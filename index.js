fdb22416ef849336a5d31ab0fb97fe1cc393964d
aur3el <103524562+aur3el@users.noreply.github.com>
2022-04-13 06:50:28 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-require('./server')
-const MusicBot = require("./structures/MusicClient");
-const client = new MusicBot();
-
-client.connect()
-
-module.exports = client; 
-//done 
