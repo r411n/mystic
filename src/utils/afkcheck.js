94ac62e54b5aa637a81866dd6cb70dcfcba41b9b
aur3el <103524562+aur3el@users.noreply.github.com>
2022-04-13 06:23:42 -0700

    Add files via upload

--- src/utils/afkcheck.js
+++ src/utils/afkcheck.js
@@ -1,1 +1,26 @@
+const client = require('../../index.js')
+const afkSchema = require('../../schema/afkSchema')
+const Discord = require('discord.js')
 
+client.on('messageCreate', async(message) => {
+    if(message.author.bot) return;
+
+
+
+        const data = await afkSchema.findOne({
+          Guild: message.guild.id
+        });
+
+        if(data) {
+          const mentionedUser = message.mentions.users.first()
+
+          if(mentionedUser.user.id == data.User)
+            const embed = new Discord.MessageEmbed()
+            .setTitle(`${mentionedUser.user.tag} is currently AFK!`)
+            .setColor("RANDOM")
+            .setDescription(`Reason: ${data.Reason} \n Since: <t:${Math.round(data.Date / 1000)}:R>`)
+
+            return message.channel.send({embeds: [embed]})
+        }
+    
+})
