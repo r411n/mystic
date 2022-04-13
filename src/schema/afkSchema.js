93716c48733e8f3b54c1867e0fb3c2025d69e842
aur3el <103524562+aur3el@users.noreply.github.com>
2022-04-13 06:16:29 -0700

    Add files via upload

--- src/schema/afkSchema.js
+++ src/schema/afkSchema.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose')
 
+const afkSchema = mongoose.Schema(
+{
+   Guild: String,
+   User: String,
+   Reason: String,
+   Date: String
+
+},
+)
+
+
+module.exports = mongoose.model('afk', afkSchema)
