b0a0ab65dff36b95fc6c1d70c79a3c098a2a3d12
aur3el <103524562+aur3el@users.noreply.github.com>
2022-04-13 06:34:49 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
     token: process.env.TOKEN || "",  // your bot token
     prefix: process.env.PREFIX || "%", // bot prefix
     ownerID: process.env.OWNERID || "755566952449310842", //your discord id
-    mongourl: process.env.MONGO_URI || "mongodb+srv://rlx:rlx@rlx.iv0gv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // MongoDb URL
+    mongourl: process.env.MONGO_URI || "mongodb+srv://mystic:mystic@cluster0.vtpde.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // MongoDb URL
     embedColor: process.env.COlOR || "#FFFFFF", // embed colour
     logs: process.env.LOGS || "961281238738534490", // channel id for guild create and delete logs
 
