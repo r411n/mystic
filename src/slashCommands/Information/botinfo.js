0b710b433725ea57978f6d5a741be999de99c589
aur3el <103524562+aur3el@users.noreply.github.com>
2022-04-14 16:54:47 +0300

    Update botinfo.js

--- src/slashCommands/Information/botinfo.js
+++ src/slashCommands/Information/botinfo.js
@@ -22,9 +22,9 @@ client.guilds.cache.forEach((guild) => {
 })
       const embed = new MessageEmbed()
       .setTitle("About Me")
-      .addField("**Bot**","```yml\nName: Mystic Music#9773[910792227591163945]\nDiscord.js: v13.5.0\nNode.js: 16.6.1\n```")
+      .addField("**Bot**","```yml\nName: @Olivia ✨#1700[924588675155755039]\nDiscord.js: v13.5.0\nNode.js: 16.6.1\n```")
      .addField("**General**","```yml\nServers: " + client.guilds.cache.size+" Servers\nUsers: " + mcount+" Users\n```")
-     .addField("**Developers**","```yml\nName: IMMORTAL [senpai]#0001 [928158667017191454] \nName: </JARVIS>#0773 [902883391421153290]```")
+     .addField("**Developers**","```yml\nName: gzR41N#9332 [755566952449310842] \nName: @Aurel#3353 [915956372204552233]```")
       await interaction.followUp({embeds: [embed]})
     }
 }
