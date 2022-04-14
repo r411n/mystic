58105a954cb6d31f8d53b0f30bf1dc16adc72168
aur3el <103524562+aur3el@users.noreply.github.com>
2022-04-14 16:56:37 +0300

    Update invite.js

--- src/slashCommands/Information/invite.js
+++ src/slashCommands/Information/invite.js
@@ -21,7 +21,7 @@ module.exports = {
     new MessageButton()
     .setLabel("Invite")
     .setStyle("LINK")
-    .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=applications.commands%20bot`),
+    .setURL(`https://discord.com/api/oauth2/authorize?client_id=924588675155755039&permissions=8&scope=bot%20applications.commands`),
 			new MessageButton()
     .setLabel("Source Code")
     .setStyle("LINK")
@@ -29,14 +29,14 @@ module.exports = {
     new MessageButton()
     .setLabel("Support")
     .setStyle("LINK")
-    .setURL("https://discord.gg/35QdxGX8se")
+    .setURL("https://discord.gg/UybeVq66hF")
 			);
 
           const mainPage = new MessageEmbed()
-            .setAuthor('Mystic Music', 'https://cdn.discordapp.com/avatars/910792227591163945/3bb282a8f43c062a8490222a96d3dde3.webp?size=1024')
+            .setAuthor('Olivia ✨', 'https://cdn.discordapp.com/avatars/910792227591163945/3bb282a8f43c062a8490222a96d3dde3.webp?size=1024')
             .setThumbnail('https://cdn.discordapp.com/avatars/910792227591163945/3bb282a8f43c062a8490222a96d3dde3.webp?size=1024')
              .setColor('#303236')
-            .addField('Invite Mystic Music', `[Here](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=applications.commands%20bot)`, true)
+            .addField('Invite Olivia', `[Here](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=36768832&scope=applications.commands%20bot)`, true)
            await interaction.followUp({embeds: [mainPage], components: [row]})
     }
 }
