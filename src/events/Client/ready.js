40d1aa3566879239a2a469ad0295dd743d36087a
aur3el <103524562+aur3el@users.noreply.github.com>
2022-04-14 16:15:47 +0300

    Update ready.js

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -8,7 +8,7 @@ run: async (client) => {
     client.logger.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`, "ready");
 
     //Game
-    let statuses = ['/help', `Prefix ${prefix}`, `@Mystic Music`];
+    let statuses = ['/help', `Prefix ${prefix}`, `@Olivia`];
     setInterval(function() {
   		let status = statuses[Math.floor(Math.random()*statuses.length)];
   		client.user.setActivity(status, {type: "LISTENING"});
