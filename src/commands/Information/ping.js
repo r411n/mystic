982af8fe9cdbec3fc3dcb17e97ed746085602e67
aur3el <103524562+aur3el@users.noreply.github.com>
2022-04-14 15:36:04 +0300

    Update ping.js

--- src/commands/Information/ping.js
+++ src/commands/Information/ping.js
@@ -10,20 +10,20 @@ module.exports = {
     owner: false,
     execute: async (message, args, client, prefix) => {
       
-  await message.reply({ content: "<a:loading:920666789459951656> Pinging..." }).then(async (msg) => {
+  await message.reply({ content: "🌀 Pinging..." }).then(async (msg) => {
   const ping = msg.createdAt - message.createdAt;
   const api_ping = client.ws.ping;
 
   const PingEmbed = new MessageEmbed()
     .setAuthor({ name: "Pong", iconURL: client.user.displayAvatarURL()})
     .setColor(client.embedColor)
-    .addField("Bot Latency <a:nping:936881101656444938>", `\`\`\`ini\n[ ${ping}ms ]\n\`\`\``, true)
-    .addField("API Latency <a:nping:936881101656444938>", `\`\`\`ini\n[ ${api_ping}ms ]\n\`\`\``, true)
+    .addField("Bot Latency", `\`\`\`ini\n[ ${ping}ms ]\n\`\`\``, true)
+    .addField("API Latency", `\`\`\`ini\n[ ${api_ping}ms ]\n\`\`\``, true)
     .setFooter({ text: `Requested by ${message.author.username}`, iconURL:  message.author.avatarURL({ dynamic: true })})
     .setTimestamp();
 
   await msg.edit({
-    content: "**Here The Mytic's Ping **\<a:nping:936881101656444938>",
+    content: "**Here The Olivia's Ping **\🏓",
     embeds: [PingEmbed]
   })
  })
